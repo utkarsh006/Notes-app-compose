36010189872cb6c0778fb095c850fe2957523732
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2023-08-19 21:18:10 +0530

    build successful

--- app/src/main/java/com/example/notesappcompose/feature_note/data/data_source/NoteDB.kt
+++ app/src/main/java/com/example/notesappcompose/feature_note/data/data_source/NoteDB.kt
@@ -1,8 +1,10 @@
 package com.example.notesappcompose.feature_note.data.data_source
 
-import android.provider.ContactsContract.CommonDataKinds.Note
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.notesappcompose.feature_note.domain.model.Note
 
 @Database(
     entities = [Note::class],
