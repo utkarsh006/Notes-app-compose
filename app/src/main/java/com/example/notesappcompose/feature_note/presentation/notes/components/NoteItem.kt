991c2b9729460e1d73540007e2db4dc71c35331f
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2023-11-09 11:02:31 +0530

    canvas removed

--- app/src/main/java/com/example/notesappcompose/feature_note/presentation/notes/components/NoteItem.kt
+++ app/src/main/java/com/example/notesappcompose/feature_note/presentation/notes/components/NoteItem.kt
@@ -1,6 +1,6 @@
 package com.example.notesappcompose.feature_note.presentation.notes.components
 
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,89 +13,60 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.core.graphics.ColorUtils
 import com.example.notesappcompose.feature_note.domain.model.Note
 
 @Composable
 fun NoteItemUI(
     note: Note,
     modifier: Modifier = Modifier,
-    cornerRadius: Dp = 10.dp,
-    cutCornerSize: Dp = 30.dp,
     onDeleteClicked: () -> Unit,
     onShareClicked: (String) -> Unit
 ) {
-    Box(modifier = modifier) {
-        Canvas(modifier = Modifier.matchParentSize()) {
-            //define a rectangle path, which would be cut at one corner 
-            val clipPath = Path().apply {
-                lineTo(size.width - cutCornerSize.toPx(), 0f)
-                lineTo(size.width, cutCornerSize.toPx())
-                lineTo(size.width, size.height)
-                lineTo(0f, size.height)
-                close()
-            }
+    Card(modifier = modifier) {
 
-            clipPath(clipPath) {
-                drawRoundRect(
-                    color = Color(note.color),
-                    size = size,
-                    cornerRadius = CornerRadius(cornerRadius.toPx())
-                )
-
-                drawRoundRect(
-                    color = Color(
-                        ColorUtils.blendARGB(note.color, 0x000000, 0.2f)
-                    ),
-                    topLeft = Offset(size.width - cutCornerSize.toPx(), -100f),
-                    size = Size(cutCornerSize.toPx() + 100f, cutCornerSize.toPx() + 100f),
-                    cornerRadius = CornerRadius(cornerRadius.toPx())
-                )
-            }
-        }
-
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp)
+                .background(Color(note.color))
         ) {
-            Text(
-                text = note.title,
-                style = MaterialTheme.typography.bodyLarge,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.secondary,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
+            Column(
+                modifier = Modifier
 
-            Spacer(modifier = Modifier.height(8.dp))
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = note.title,
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.secondary,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
 
-            Text(
-                text = note.content,
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.background,
-                maxLines = 10,
-                overflow = TextOverflow.Ellipsis
-            )
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(
+                    text = note.content,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.background,
+                    maxLines = 10,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
         }
-
 
     }
 
@@ -139,8 +110,6 @@ fun NoteItemUIPreview() {
             1
         ),
         modifier = Modifier.padding(16.dp),
-        cornerRadius = 8.dp,
-        cutCornerSize = 20.dp,
         onDeleteClicked = {},
         onShareClicked = {}
     )
